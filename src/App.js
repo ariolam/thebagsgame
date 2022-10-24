d44afd373eeaf3d1d624ec2ffa48ee94d66ca170
ariola <ariola.markou@gmail.com>
2022-10-24 17:49:55 +0300

    Delete the logo and styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>The Bags Game</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>The Bags Game</h3>
     </div>
   );
 }
