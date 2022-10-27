d3f99ae8e0c58d76dd98aea5b0bc769a16ea804e
ariola <ariola.markou@gmail.com>
2022-10-27 15:35:30 +0300

    Added the image url and desc

--- src/data.js
+++ src/data.js
@@ -1,33 +1,38 @@
 const bags = [
   {
     id: 1,
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1601924921557-45e6dea0a157?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
     type: "Handbag",
-    desc: "",
+    desc: "Red Valentiono handbag in leather",
   },
   {
     id: 2,
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1566958799193-c2aa57a835d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1846&q=80",
     type: "Clutch",
-    desc: "",
+    desc: "Black clucth",
   },
   {
     id: 3,
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1538561690977-f076de7e4fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
     type: "Wallet",
-    desc: "",
+    desc: "Blue wallet",
   },
   {
     id: 4,
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1548036328-c9fa89d128fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80",
     type: "Crossbody",
-    desc: "",
+    desc: "Black gucci crossbody bag in leather",
   },
   {
     id: 5,
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1522336607782-36f5d43c2b51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
     type: "Shopper",
-    desc: "",
+    desc: "Purple and white shopper bag",
   },
 ];
 
