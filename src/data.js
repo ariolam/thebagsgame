d9d3decb9f9d592923cb2050ed4e87610f24ab4f
ariola <ariola.markou@gmail.com>
2022-10-27 14:04:42 +0300

    Created the json with ids and types

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const bags = [
+  {
+    id: 1,
+    image: "",
+    type: "Handbag",
+    desc: "",
+  },
+  {
+    id: 2,
+    image: "",
+    type: "Clutch",
+    desc: "",
+  },
+  {
+    id: 3,
+    image: "",
+    type: "Wallet",
+    desc: "",
+  },
+  {
+    id: 4,
+    image: "",
+    type: "Crossbody",
+    desc: "",
+  },
+  {
+    id: 5,
+    image: "",
+    type: "Shopper",
+    desc: "",
+  },
+];
+
+export default bags;
