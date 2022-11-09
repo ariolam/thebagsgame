cec4c429c64cfc75cfcfc4110bbb138285d583b4
ariola <ariola.markou@gmail.com>
2022-11-09 15:15:14 +0200

    import data in the page

--- src/data.js
+++ src/data.js
@@ -3,36 +3,56 @@ const bags = [
     id: 1,
     image:
       "https://images.unsplash.com/photo-1601924921557-45e6dea0a157?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-    type: "Handbag",
     desc: "Red Valentiono handbag in leather",
+    options: [
+      { id: 0, text: "handbag", isCorrect: true },
+      { id: 1, text: "tote bag", isCorrect: false },
+      { id: 2, text: "shopper", isCorrect: false },
+    ],
   },
   {
     id: 2,
     image:
       "https://images.unsplash.com/photo-1566958799193-c2aa57a835d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1846&q=80",
-    type: "Clutch",
     desc: "Black clucth",
+    options: [
+      { id: 0, text: "handbag", isCorrect: false },
+      { id: 1, text: "crossbody", isCorrect: false },
+      { id: 2, text: "clutch", isCorrect: true },
+    ],
   },
   {
     id: 3,
     image:
       "https://images.unsplash.com/photo-1538561690977-f076de7e4fd8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-    type: "Wallet",
     desc: "Blue wallet",
+    options: [
+      { id: 0, text: "tote bag", isCorrect: false },
+      { id: 1, text: "wallet", isCorrect: true },
+      { id: 2, text: "shopper", isCorrect: false },
+    ],
   },
   {
     id: 4,
     image:
       "https://images.unsplash.com/photo-1548036328-c9fa89d128fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80",
-    type: "Crossbody",
     desc: "Black gucci crossbody bag in leather",
+    options: [
+      { id: 0, text: "crossdoby", isCorrect: true },
+      { id: 1, text: "tote bag", isCorrect: false },
+      { id: 2, text: "wallet", isCorrect: false },
+    ],
   },
   {
     id: 5,
     image:
       "https://images.unsplash.com/photo-1522336607782-36f5d43c2b51?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
-    type: "Shopper",
     desc: "Purple and white shopper bag",
+    options: [
+      { id: 0, text: "handbag", isCorrect: false },
+      { id: 1, text: "tote bag", isCorrect: false },
+      { id: 2, text: "shopper", isCorrect: true },
+    ],
   },
 ];
 
